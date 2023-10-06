8e16dc05deb1e0a966b5ba775c7d9d6b27499de6
ankita tripathy <in_kumarit@m1.com.sg>
2023-10-06 10:58:02 +0530

    Task-3 done testing

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -15,6 +15,10 @@ describe('When: I use the reading list feature', () => {
 
 it('Then: I should set the reading list state back to the previous state',()=>{
   cy.get('[data-testing="want_to_read_btn"]').click();
-  
-  cy.get('[data-testing="want_to_read_btn"]').should('');
+
+  cy.wait(4000);
+
+  cy.contains('Undo').click();
+
+  cy.get('[data-testing="reading-item"]').should('have.length',0);
 })
